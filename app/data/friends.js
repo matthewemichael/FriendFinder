4f1829fb4f834b36a493dbd0df85aca6a243d421
Matt Michael <matthewemichael@gmail.com>
2019-10-24 14:29:10 -0500

    updated friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,31 +2,196 @@ var friendArray = [{
     name: "Barney Gumble",
     photo: "images/barney-gumble.png",
     scores: [
-        "5",
+        "4",
         "2",
-        "5",
         "1",
-        "4",
-        "4",
+        "1",
+        "5",
         "3",
-        "2",
+        "4",
         "1",
-        "1"
+        "1",
+        "3"
     ]
 }, {
     name: "Bart Simpson",
     photo: "images/bart-simpson.png",
     scores: [
+        "4",
+        "1",
+        "1",
+        "5",
+        "2",
+        "2",
+        "4",
+        "4",
+        "1",
+        "2"
+    ]
+}, {
+    name: "Chief Wiggum",
+    photo: "images/chief-wiggum.png",
+    scores: [
         "3",
         "1",
+        "3",
+        "5",
+        "4",
+        "2",
+        "4",
+        "4",
+        "1",
+        "1"
+    ]
+}, {
+    name: "Homer Simpson",
+    photo: "images/homer-simpson.png",
+    scores: [
+        "5",
+        "3",
+        "1",
+        "2",
+        "5",
+        "2",
+        "3",
+        "3",
+        "2",
+        "2"
+    ]
+}, {
+    name: "Lisa Simpson",
+    photo: "images/lisa-simpson.png",
+    scores: [
+        "4",
+        "1",
+        "5",
+        "1",
+        "1",
+        "5",
+        "1",
+        "4",
+        "5",
+        "4"
+    ]
+}, {
+    name: "Marge Simpson",
+    photo: "images/marge-simpson.png",
+    scores: [
+        "2",
+        "2",
+        "2",
+        "1",
+        "1",
+        "2",
+        "1",
+        "3",
+        "4",
+        "4"
+    ]
+}, {
+    name: "Milhouse Van Houten",
+    photo: "images/milhouse-van-houten.png",
+    scores: [
+        "2",
+        "1",
+        "1",
+        "1",
+        "3",
+        "3",
+        "3",
+        "2",
+        "1",
+        "2"
+    ]
+}, {
+    name: "Moe Szyslak",
+    photo: "images/moe-szyslak.png",
+    scores: [
+        "1",
+        "1",
+        "1",
+        "3",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+    ]
+}, {
+    name: "Mr. Burns",
+    photo: "images/mr-burns.png",
+    scores: [
+        "3",
+        "3",
+        "4",
+        "5",
+        "1",
+        "1",
+        "1",
+        "5",
+        "5",
+        "1"
+    ]
+}, {
+    name: "Ned Flanders",
+    photo: "images/ned-flanders.png",
+    scores: [
+        "2",
+        "4",
+        "1",
+        "1",
+        "1",
+        "2",
+        "1",
+        "3",
+        "5",
+        "5"
+    ]
+}, {
+    name: "Nelson Muntz",
+    photo: "images/nelson-muntz.png",
+    scores: [
+        "1",
+        "2",
+        "1",
+        "4",
+        "2",
+        "1",
+        "2",
+        "2",
+        "2",
+        "1"
+    ]
+}, {
+    name: "Ralph Wiggum",
+    photo: "images/ralph-wiggum.png",
+    scores: [
         "5",
         "3",
         "2",
-        "4",
-        "3",
         "1",
         "2",
+        "2",
+        "5",
+        "1",
+        "1",
         "4"
+    ]
+}, {
+    name: "Waylon Smithers",
+    photo: "images/waylon-smithers.png",
+    scores: [
+        "5",
+        "5",
+        "5",
+        "1",
+        "3",
+        "4",
+        "2",
+        "4",
+        "5",
+        "3"
     ]
 }];
 
